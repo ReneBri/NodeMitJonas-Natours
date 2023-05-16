e71cb04c748bfc17ee54086a746580c7d9e6441b
Rene Bridgman <renebridgman1@gmail.com>
2023-05-16 20:36:42 +0200

    CRUD functions for tours through our controllers

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,65 +2,76 @@ const Tour = require('./../models/tourModel');
 
 // tour route functions/controllers
 
-let tours = [];
+// let tours = [];
 
-Tour.find({}).then(results => {
-    tours = results;
-});
+// Tour.find({}).then(results => {
+//     tours = results;
+// });
 
-exports.checkBody = (req, res, next) => {
-    const { body } = req;
-    if (!body.name || body.name.length < 1) {
-        return res.status(400).json({
-            status: 'fail',
-            message: 'Please provide a details for the name field.'
-        });
-    }
-
-    if (!body.price || body.price.length < 1) {
-        return res.status(400).json({
-            status: 'fail',
-            message: 'Please provide a details for the price field.'
-        });
-    }
-    next();
-};
-
-exports.getAllTours = (req, res) => {
-    // this is the JSend standard of sending a response.
-    // the results part however, is not part of that standard.
-    res.status(200).json({
-        status: 'success',
-        results: tours.length,
-        tours
-    });
-};
-
-exports.createTour = (req, res) => {
-    res.status(201).json({
-        status: 'success'
-        // data: {
-        //     tour: newTour
-        // }
-    });
-};
-
-exports.getTour = (req, res) => {
-    const id = +req.params.id;
-    const tour = tours.find(tour => tour.id === id);
-    if (tour) {
+exports.getAllTours = async (req, res) => {
+    try {
+        const tours = await Tour.find({});
+        // this is the JSend standard of sending a response.
+        // the results part however, is not part of that standard.
         res.status(200).json({
             status: 'success',
-            data: {
-                tour
-            }
+            results: tours.length,
+            tours
         });
-    } else {
+    } catch (err) {
         res.status(404).json({
-            status: 'failed',
-            message: 'Sorry, this doesnt exist.'
+            status: 'fail',
+            message: err.message
         });
     }
+};
+
+exports.createTour = async (req, res) => {
+    // const { body } = req;
+    try {
+        const savedTour = await Tour.create(req.body);
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: savedTour
+            }
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: `Failed to save - ${err.message}`
+        });
+    }
+};
+
+exports.getTour = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const foundTour = await Tour.findById(id);
+        if (foundTour) {
+            res.status(200).json({
+                status: 'success',
+                data: foundTour
+            });
+        } else {
+            throw new Error('No data found');
+        }
+    } catch (err) {
+        res.status(404).json({
+            status: 'failed',
+            message: err.message
+        });
+    }
+
+    // const tour = tours.find(tour => tour.id === id);
+    // if (tour) {
+    //     r
+    // } else {
+    //     res.status(404).json({
+    //         status: 'failed',
+    //         message: 'Sorry, this doesnt exist.'
+    //     });
+    // }
 };
 
 exports.updateTour = (req, res) => {
